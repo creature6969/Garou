3aca2bc261cd1ed42ebf74784fbff5b299fc7c9a
creature6969 <159709673+creature6969@users.noreply.github.com>
2024-03-12 17:30:23 +0530

    Update good_morning.py

--- MukeshRobot/modules/good_morning.py
+++ MukeshRobot/modules/good_morning.py
@@ -36,7 +36,7 @@ async def awake(event):
     NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ❀\n\n✦ ᴡᴇʟᴄᴏᴍᴇ ᴛʜɪs ʙᴇᴀᴜᴛɪғᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ. I ʜᴏᴘᴇ ʏᴏᴜ ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [๛ᴀ ᴠ ɪ s ʜ ᴀ ༗](https://t.me/avishaxbot)"
     BUTTON = [
         [
-            Button.url("ᴍᴇᴇᴛ ᴍᴇ ʜᴇʀᴇ ʙᴀʙʏ", "https://telegram.dog/The_Friendz"),
+            Button.url("⛩ ᴀᴅᴅ ɢᴀʀᴏᴜ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⛩", "https://t.me/garouproXbot?startgroup=true"),
         ]
     ]
     await telethn.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
